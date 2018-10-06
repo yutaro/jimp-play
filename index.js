2c9ada36bb4865d4b3db33702217d0a300755aa0
yutaro <llgoodrr@gmail.com>
2018-10-06 21:55:58 +0900

    add samples

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ main()
 
 async function main() {
     const image = await jimp.read('./src/yutaro.jpg')
-    image.resize(256, 256)
-        .write('./image/resize.png')
-    image.sepia()
-        .write('./image/sepia.png')
-    image.color([{ apply: 'red', params: [100] }])
-        .write('./image/red_100.png')
+    const processes = {
+        resize: img => img.resize(255, 255),
+        sepia:  img => img.sepia(),
+    }
+
+    for (let i = -90; i <= 90; i += 10) {
+        processes[`hue/${i}`] = img => img.color([{ apply: 'hue', params: [i] }])
+        processes[`saturate/${i}`] = img => img.color([{ apply: 'saturate', params: [i] }])
+        processes[`lighten/${i}`] = img => img.color([{ apply: 'lighten', params: [i] }])
+        processes[`darken/${i}`] = img => img.color([{ apply: 'darken', params: [i] }])
+    }
+
+    for( const label in processes ) {
+        process(image, label, processes[label])
+    }
+}
+
+
+async function process(image, label, callback) {
+    const img = image.clone()
+    callback(img)
+        .write(`./image/${label}.png`)
 }
